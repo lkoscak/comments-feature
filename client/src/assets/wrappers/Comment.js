fd796bffd2792c5fb7a31531cbb760eb74711123
Luka Košćak <luka.koscak96@gmail.com>
2023-09-15 15:29:05 +0200

    Basic layout for comments view section implemented

--- client/src/assets/wrappers/Comment.js
+++ client/src/assets/wrappers/Comment.js
@@ -1,5 +1,84 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+	width: 100%;
+	margin-bottom: 30px;
+
+	.container {
+		max-width: 80%;
+		display: flex;
+		justify-content: flex-start;
+		gap: 10px;
+		margin-left: ${({ isReply }) => (isReply ? "60px" : "0px")};
+		.avatar-container {
+			align-self: flex-start;
+			height: 100%;
+
+			.avatar {
+				height: 48px;
+				width: 48px;
+				border-radius: 50%;
+				border: 0.5px solid var(--color-bg-secondaryLight);
+			}
+
+			.avatar.no-img {
+				background-color: var(--color-bg-primaryDark);
+			}
+		}
+
+		.comment {
+			flex: 0 1 auto;
+			display: flex;
+			flex-direction: column;
+			justify-content: flex-start;
+			gap: 5px;
+
+			.content {
+				display: flex;
+				flex-direction: column;
+				justify-content: flex-start;
+				border-radius: 8px;
+				border: 1px solid #d8dee7;
+				background-color: var(--color-bg-secondaryLight);
+				gap: 10px;
+				padding: 20px;
+
+				h3,
+				p {
+					margin: 0;
+				}
+
+				.title {
+					font-weight: 600;
+					font-size: 18px;
+					color: var(--color-text-primaryDark);
+				}
+
+				.text {
+					font-weight: 500;
+					font-size: 18pxpx;
+					color: var(--color-text-primaryLight);
+				}
+			}
+
+			.actions {
+				display: flex;
+				align-items: center;
+				gap: 10px;
+				font-size: 16px;
+				font-weight: 500;
+
+				.timestamp {
+					color: var(--color-text-primaryLight);
+				}
+				.action-reply {
+					cursor: pointer;
+					color: var(--color-bg-primaryDark);
+					font-weight: 600;
+				}
+			}
+		}
+	}
+`;
 
 export default Wrapper;
--- client/src/components/comments_display/Comment.jsx
+++ client/src/components/comments_display/Comment.jsx
@@ -2,14 +2,47 @@ import Wrapper from "../../assets/wrappers/Comment";
 import moment from "moment/moment";
 
 const Comment = ({ comment }) => {
+	const commentDateTime = comment.timestamp
+		? moment.unix(comment.timestamp).format("LT")
+		: "";
 	return (
-		<Wrapper>
-			<div>{comment.text}</div>
+		<>
+			<Wrapper isReply={!comment.replies}>
+				<div className="container">
+					<div className="avatar-container">
+						{comment.author.picture ? (
+							<img
+								className="avatar"
+								src={`src/assets/images/${comment.author.picture}`}
+							/>
+						) : (
+							<div className="avatar no-img"></div>
+						)}
+					</div>
+					<div className="comment">
+						<div className="content">
+							<h3 className="title">{comment.author.name}</h3>
+							<p className="text">{comment.text}</p>
+						</div>
+						<div className="actions">
+							<span className="timestamp">{commentDateTime}</span>
+							<span>&#183;</span>
+							<div className="actions">
+								<span className="action-reply">{`Reply${
+									comment.replies && comment.replies.length > 0
+										? `(${comment.replies.length})`
+										: ""
+								}`}</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</Wrapper>
 			{comment.replies &&
 				comment.replies.map((reply) => {
 					return <Comment key={reply.id} comment={reply}></Comment>;
 				})}
-		</Wrapper>
+		</>
 	);
 };
 
